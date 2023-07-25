729f17f1e0f5bc863034361fa44f600fbeb50c88
Abdul Mannan <mannansaudi@gmail.com>
2023-07-25 17:14:58 +0530

    fixed axios error

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import bodyparser from 'body-parser';
 import cors from 'cors';
-const https = require('https');
+import axios from 'axios';
 require('dotenv').config();
+
 const app = express();
 const PORT = process.env.PORT || 4000;
 
@@ -17,52 +18,35 @@ const corsOptions = {
 }
 app.use(cors(corsOptions));
 
-// app.get('/', async function (req, res) {
-//     try {
-//         const response = await axios.get(
-//             `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CX}&q=${req.query.q}`
-//         );
-//         res.json(response.data);
-//     } catch (error) {
-//         res.send(error);
-//     }
-// })
-
-function getSearchResults(options) {
-
-    return new Promise((resolve, reject) => {
-        https.get(options, response => {
-            let data = '';
-
-            response.on('data', chunk => {
-                data += chunk;
-            });
-
-            response.on('end', () => {
-                resolve(JSON.parse(data));
-            });
-        }).on('error', error => {
-            reject(error);
-        });
-    });
-}
-
 app.get('/', async function (req, res) {
 
     try {
-        const options = {
-            hostname: 'www.googleapis.com',
-            path: `/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CX}&q=${req.query.q}`
-        };
-        const searchResults = await getSearchResults(options);
+        const response = await axios.get('https://www.googleapis.com/customsearch/v1', {
+            params: {
+                key: process.env.CS_API_KEY,
+                cx: process.env.CX,
+                q: req.query.q
+            },
+        });
+        const searchResults = response.data;
         const links = searchResults.items.slice(0, 5).map(item => item.link);
         res.json(links);
+        // const links = [
+        //     "https://en.wikipedia.org/wiki/Banana",
+        // ]
+
+        // axios.get('https://app.scrapingbee.com/api/v1', {
+        //     params: {
+        //         'api_key': '5IM18AO798PYGG3NLF6FMNRDZZ4532QH8GDNATJZXH99I90YK0L0VH3HFGTOU5OK16TX3B16JTQJA7TL',
+        //         'url': 'http://httpbin.scrapingbee.com/anything?json',
+        //     }
+        // }).then(function (response) {
+        //     console.log(response);
+        // })
     } catch (error) {
         res.send(error);
     }
 });
-
-
 
 app.listen(PORT, () =>
     console.log(`Server running on port ${PORT}`)
