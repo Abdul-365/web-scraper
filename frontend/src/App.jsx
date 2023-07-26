5c863ec7fcd9f060560f3d524f20be1e908ce946
Abdul Mannan <mannansaudi@gmail.com>
2023-07-26 12:58:05 +0530

    designed basic

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import axios from "axios";
+
+export default function App() {
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const [results, setResults] = useState([]);
+
+    const handleSearch = (e) => {
+        setSearchTerm(e.target.value)
+    }
+
+
+    function getResults(e) {
+        e.preventDefault();
+        axios.get(process.env.REACT_APP_API_URL, { params: { q: searchTerm } })
+            .then(({ data }) => setResults(data))
+            .catch(error => console.log(error));
+    }
+
+    return (
+        <>
+            <input type="text" placeholder="Search" value={searchTerm} onChange={handleSearch} />
+            <button onClick={getResults} type='Submit'>Search</button>
+            {results.map(result => <p>{result}</p>)}
+        </>
+    );
+}
