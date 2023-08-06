e3b0724114c62a0dae7b0e8d7618d4ffc7fa4bba
Abdul Mannan <mannansaudi@gmail.com>
2023-08-06 13:03:51 +0530

    frontend test deployment

--- backend/functions/api.js
+++ backend/functions/api.js
@@ -21,11 +21,11 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 
-app.get('/', function (req, res) {
+app.get('/api', function (req, res) {
     res.send('Hello World!');
 })
 
-app.get('/search', async function (req, res) {
+app.get('/api/search', async function (req, res) {
 
     try {
         let response = await axios.get('https://www.googleapis.com/customsearch/v1', {
